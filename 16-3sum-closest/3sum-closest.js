b9b685bfd07a6065b52c5a7acdbef324f4c506ef
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-11-11 21:46:50 +0700

    Time: 22 ms (12.04%) | Memory: 57.6 MB (37.73%) - LeetSync

--- 16-3sum-closest/3sum-closest.js
+++ 16-3sum-closest/3sum-closest.js
@@ -5,7 +5,6 @@
  */
 var threeSumClosest = function(nums, target) {
     nums.sort((a, b) => a - b)
-    let res = []
     let closest = Infinity
 
     for(let i = 0; i < nums.length; i++){
@@ -14,15 +13,16 @@ var threeSumClosest = function(nums, target) {
         let l = i + 1, r = nums.length - 1
         while(l < r){
             total = nums[i] + nums[l] + nums[r]
-            current_dist = Math.abs(target - total)
+            current_dist = Math.abs(total - target)
             closest_dist = Math.abs(target - closest)
 
-            if(current_dist < closest_dist)
+            if(current_dist < closest_dist){
                 closest = total
-            
-            if (total > target) r --
+            }
+
+            if (total > target) r--
             else if (total < target) l ++
-            else return total
+            else return total 
         }
     }
     return closest
