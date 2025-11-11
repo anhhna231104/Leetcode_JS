3bf59a65be35c3ce96be9451b7fed76e0f7f9b23
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-11-11 21:43:10 +0700

    Time: 18 ms (17.12%) | Memory: 57.7 MB (20.40%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b)
+    let res = []
+    let closest = Infinity
+
+    for(let i = 0; i < nums.length; i++){
+        if(i > 0 && nums[i] === nums[i - 1]) continue
+
+        let l = i + 1, r = nums.length - 1
+        while(l < r){
+            total = nums[i] + nums[l] + nums[r]
+            current_dist = Math.abs(target - total)
+            closest_dist = Math.abs(target - closest)
+
+            if(current_dist < closest_dist)
+                closest = total
+            
+            if (total > target) r --
+            else if (total < target) l ++
+            else return total
+        }
+    }
+    return closest
+};
