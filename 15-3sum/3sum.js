eef51166a3f0ba434e3ad359c37aa071e383948b
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-11-11 21:34:21 +0700

    Time: 35 ms (69.83%) | Memory: 72 MB (75.07%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -8,20 +8,20 @@ var threeSum = function(nums) {
 
     for(let i = 0; i < nums.length; i++)
     {
-        if (i > 0 && nums[i] == nums[i - 1]) continue
+        if (i > 0 && nums[i] === nums[i - 1]) continue
 
-        l = i + 1, r = nums.length - 1
+        let l = i + 1, r = nums.length - 1
         while (l < r)
         {
             total = nums[i] + nums[l] + nums[r]
 
-            if (total < 0) l += 1
-            else if (total > 0) r -= 1
+            if (total < 0) l ++
+            else if (total > 0) r --
             else {
                 res.push([nums[i], nums[l], nums[r]])
-                l += 1
+                l ++
                 while (nums[l] == nums[l - 1] && l < r)
-                    l += 1
+                    l ++
             }          
         }
     }
