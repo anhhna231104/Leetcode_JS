e537bfe49d520a2b5813ca372af0938cfd6270b5
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-12-12 15:21:07 +0700

    Time: 24 ms (36.59%) | Memory: 14.4 MB (13.56%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        counterS, counterT = {}, {}
+        for i in range(len(s)):
+            counterS[s[i]] = 1 + counterS.get(s[i], 0)
+            counterT[t[i]] = 1 + counterT.get(t[i], 0)
+        
+        for c in counterS:
+            if counterS[c] != counterT.get(c, 0):
+                return False
+        
+        return True
+
+
+        
