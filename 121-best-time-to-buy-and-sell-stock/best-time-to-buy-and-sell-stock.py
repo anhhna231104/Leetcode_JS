56123dc4a09ef992846e9865dc5dcb6125a63d9e
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-12-15 23:34:36 +0700

    Time: 119 ms (33.10%) | Memory: 19 MB (71.66%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -10,10 +10,11 @@ class Solution(object):
         while r < len(prices):
             if prices[l] < prices[r]:
                 profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
+                max_profit = max(profit, max_profit)
             else:
                 l = r
             r += 1
         
         return max_profit
+            
         
